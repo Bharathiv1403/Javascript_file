5663af45be86078ea8e7a7680842cdb98a1d482b
Bharathv1403 <bharathiv206bsc@gmail.com>
2024-11-19 17:54:19 +0530

    problems add

---
+++ JS_File/condition2.js
@@ -0,0 +1,85 @@
+//problem 1
+
+var colour =  "green"
+if (colour=="red")
+{
+    console.log("Stop")
+}
+if (colour=="yellow")
+{
+    console.log("Get Ready")
+}
+
+if (colour=="green")
+{
+    console.log("Go")
+}
+
+// else{
+//     console.log("Try again correct colour")
+// }
+
+//problem 2
+
+var season = "spring"
+
+if (season=="spring")
+{
+    console.log("Enjoy the blooming flowers.")
+}
+if (season=="summer")
+{
+    console.log("Have fun in the sun")
+}
+if (season=="autumn")
+{
+    console.log("Admire the colorful leaves")
+}
+if (season=="winter")
+{
+    console.log("Bundie up and stay warm")
+}
+
+
+//problem 3
+var score = 85
+if (score<=50)
+{
+    console.log("You need to improve")
+}
+else if (score>50 && score<70)
+{
+    console.log("Gread job")
+}
+else if (score>70)
+{
+    console.log("Excellent Performance")
+}
+
+//problem 4
+
+var number = 26;
+if (number % 2 == 0) {
+    console.log("Even number");
+} else {
+    console.log("Odd number");
+}
+
+//problem 5
+
+var vowel="e"
+
+if (vowel=="a")
+{
+    console.log("It's a vowel")
+} else if (vowel=="e") {
+    console.log("It's a vowel")
+} else if (vowel=="i") {
+    console.log("It's a vowel")
+} else if (vowel=="o") {
+    console.log("It's a vowel")
+} else if (vowel=="u") {
+    console.log("It's a vowel")
+} else {
+    console.log("it's a consonant")
+}
---
+++ JS_File/for-loop1.js
@@ -0,0 +1,46 @@
+// //method 1
+// for (count=1;count<=7;count=count+1)
+// {
+//     console.log("Dummy")
+// }
+
+// //method 2
+// for (inc=1;inc<=10; inc=inc+1){
+//     console.log(inc)
+// }
+
+//method 3 - reverse 
+// for (inc=10;inc>=1; inc=inc-1){
+//     console.log(inc)
+// }
+
+// problem 1 - Even number 
+
+// for (even=1;even<=10;even=even+1){
+//     if (even %2 ==0){
+//         console.log(even)
+//     }
+// }
+
+//problem 2 - 2 Tables 
+
+let n = 2;
+for (let tap=1;tap<=10;tap++){
+    console.log(`${tap} * ${n} = ${tap * n}`);
+}
+
+console.log("______________________________________")
+
+let m = 3;
+for (let tap=1;tap<=10;tap++){
+    console.log(`${tap} * ${m} = ${tap * m}`);
+}
+
+console.log("______________________________________")
+
+let o = 4;
+for (let tap=1;tap<=10;tap++){
+    console.log(`${tap} * ${o} = ${tap * o}`);
+}
+
+
